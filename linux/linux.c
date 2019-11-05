3f4641c0f5014481a37a143054cde61181cc8a5e
Adrien Buot <adrien.buot@edu.ece.fr>
2019-11-05 11:38:06 +0100

    partie 4

---
+++ linux/linux.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <sys/types.h> 
+#include <unistd.h> 
+#include <sys/wait.h>
+#include <signal.h>
+
+void printMessage(int sig);
+
+int main(){
+	int stat; 
+    pid_t pid; 
+    if(fork() == 0){
+      printf("Hello\n");
+      while(1){
+      	printf("MERDE\n");
+      }
+    }
+    else
+    { 
+    	signal(SIGUSR1, printMessage);
+    //     kill(pid, SIGINT); 
+    //     wait(&stat); 
+    //    if (WIFSIGNALED(stat)) 
+    //         psignal(WTERMSIG(stat), "Child term due to"); 
+    }  
+}
+
+void printMessage(int sig){
+	printf("Signal reçu %d", sig);
+}
