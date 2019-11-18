f38b8edd8242b09b9ca398cf4039f53a3bd592a8
Lightman <leoguichar@gmail.com>
2019-11-18 09:00:05 +0100

    buzzer evolué

--- projet technologique/projet/projet final/PF_V1.0ecranrfid/PF_V1.0ecranrfid.ino
+++ projet technologique/projet/projet final/PF_V1.0ecranrfid/PF_V1.0ecranrfid.ino
@@ -6,22 +6,66 @@
 #include <SPI.h>
 #include <RFID.h>
 
-#define N1 1047
-#define N2 31
-#define N3 4978
+#define N_F3  175
+#define N_FS3 185
+#define N_G3  196
+#define N_GS3 208
+#define N_A3  220
+#define N_AS3 233
+#define N_B3  247
+#define N_C4  262
+#define N_CS4 277
+#define N_D4  294
+#define N_DS4 311
+#define N_E4  330
+#define N_F4  349
+#define N_FS4 370
+#define N_G4  392
+#define N_GS4 415
+#define N_A4  440
+#define N_AS4 466
+#define N_B4  494
+#define N_C5  523
+#define N_CS5 554
+#define N_D5  587
+#define N_DS5 622
+#define N_E5  659
+#define N_F5  698
+#define N_FS5 740
+#define N_G5  784
+#define N_GS5 831
+#define N_A5  880
+#define N_AS5 932
+#define N_B5  988
+#define N_C6  1047
+#define N_CS6 1109
 
 RFID monModuleRFID(10,9);
 LiquidCrystal_I2C lcd(0x27,20,4);  // set the LCD address to 0x27 for a 16 chars and 2 line display
 
 int UID[5];
 
+// notes in the melody:
+int melody[] = {
+  N_AS4, 0, 0, N_AS4, N_AS4, N_AS4, N_AS4, N_AS4, 0, N_GS4, N_AS4, 0, 0, N_AS4, N_AS4, N_AS4, N_AS4, N_AS4, 0, N_GS4, N_AS4, 0, 0, N_AS4, N_AS4, N_AS4, N_AS4, N_AS4, N_F3, N_F3, N_F3, N_F3, N_F3, N_F3, N_F3, N_F3, N_F3, N_F3, N_AS4, N_F3, N_F3, 0, N_AS4, N_AS4, N_C5, N_D5, N_DS5, N_F5, 0, N_F5, N_F5, N_FS5, N_GS5, N_AS5, 0, N_AS5, N_AS5, N_AS5, N_GS5, N_FS5, N_GS5, 0, N_FS5, N_F5, N_F5, N_DS5, N_DS5, N_F5, N_FS5, N_F5, N_DS5, N_CS5, N_CS5, N_DS5, N_F5, N_DS5, N_CS5, N_C5, N_C5, N_D5, N_E5, N_G5, N_F5, N_F3, N_F3, N_F3, N_F3, N_F3, N_F3, N_F3, N_F3, N_F3, N_F3, N_AS4, N_F3, N_F3, 0, N_AS4, N_AS4, N_C5, N_D5, N_DS5, N_F5, 0, N_F5, N_F5, N_FS5, N_GS5, N_AS5, 0, N_CS6, N_C6, N_A5, 0, N_F5, N_FS5, 0, N_AS5, N_A5, N_F5, 0, N_F5, N_FS5, 0, N_AS5, N_A5, N_F5, 0, N_D5, N_DS5, 0, N_FS5, N_F5, N_CS5, 0, N_AS4, N_C5, N_C5, N_D5, N_E5, 0, N_G5, N_F5, N_F3, N_F3, N_F3, N_F3, N_F3, N_F3, N_F3, N_F3, N_F3, N_F3, N_AS4, N_F3, N_F3, 0, N_AS4, N_AS4, N_C5, N_D5, N_DS5, N_F5, 0, N_F5, N_F5, N_FS5, N_GS5, N_AS5, 0, N_AS5, N_AS5, N_AS5, N_GS5, N_FS5, N_GS5, 0, N_FS5, N_F5, N_F5, N_DS5, N_DS5, N_F5, N_FS5, N_F5, N_DS5, N_CS5, N_CS5, N_DS5, N_F5, N_DS5, N_CS5, N_C5, N_C5, N_D5, N_E5, N_G5, N_F5, N_F3, N_F3, N_F3, N_F3, N_F3, N_F3, N_F3, N_F3, N_F3, N_F3, N_AS4, N_F3, N_F3, 0, N_AS4, N_AS4, N_C5, N_D5, N_DS5, N_F5, 0, N_F5, N_F5, N_FS5, N_GS5, N_AS5, 0, N_CS6, N_C6, N_A5, 0, N_F5, N_FS5, 0, N_AS5, N_A5, N_F5, 0, N_F5, N_FS5, 0, N_AS5, N_A5, N_F5, 0, N_D5, N_DS5, 0, N_FS5, N_F5, N_CS5, 0, N_AS4, N_C5, N_C5, N_D5, N_E5, 0, N_G5, N_F5, N_F3, N_F3, N_F3, N_F3, N_F3, N_F3, N_F3, N_F3, N_F3, N_F3
+};
+
+// note durations: 4 = quarter note, 8 = eighth note, etc.:
+int noteDurations[] = {
+  2, 8, 8, 8, 8, 8, 8, 6, 16, 16, 4, 8, 8, 8, 8, 8, 8, 6, 16, 16, 4, 8, 8, 8, 8, 8, 8, 8, 16, 16, 8, 16, 16, 8, 16, 16, 8, 8, 4, 4, 6, 16, 16, 16, 16, 16, 16, 2, 8, 8, 8 ,8 ,8, 2, 8, 8, 8 ,8 ,8, 8, 6, 16, 16, 2, 4, 8, 16, 16, 2, 8, 8, 8, 16, 16, 2, 8, 8, 8, 16, 16, 2, 4, 8, 16, 16, 8, 16, 16, 8, 16, 16, 8, 8, 4, 4, 6, 16, 16, 16, 16, 16, 16, 2, 8, 8, 8 ,8 ,8, 2, 4, 4, 4, 4, 4, 4, 2, 4, 4, 4, 4, 4, 4, 2, 4, 4, 4, 4, 4, 4, 2, 4, 4, 4, 4, 4, 4, 8, 16, 16, 4, 4, 4, 8, 16, 16, 8, 16, 16, 8, 16, 16, 8, 8, 4, 4, 6, 16, 16, 16, 16, 16, 16, 2, 8, 8, 8 ,8 ,8, 2, 8, 8, 8 ,8 ,8, 8, 6, 16, 16, 2, 4, 8, 16, 16, 2, 8, 8, 8, 16, 16, 2, 8, 8, 8, 16, 16, 2, 4, 8, 16, 16, 8, 16, 16, 8, 16, 16, 8, 8, 4, 4, 6, 16, 16, 16, 16, 16, 16, 2, 8, 8, 8 ,8 ,8, 2, 4, 4, 4, 4, 4, 4, 2, 4, 4, 4, 4, 4, 4, 2, 4, 4, 4, 4, 4, 4, 2, 4, 4, 4, 4, 4, 4, 8, 16, 16, 4, 4, 4, 8, 16, 16, 8, 16, 16, 8, 16, 16, 8, 8  
+};
+
+int melody_len = sizeof(melody)/sizeof(melody[0]);
+int here = 0;
+
 void setup()
 {
   lcd.init();                      // initialize the lcd 
-  lcd.init();
   // Print a message to the LCD.
   lcd.setCursor(0,0);
   lcd.print("Hello, world!");
+
+  
     SPI.begin();
   monModuleRFID.init();  
 
@@ -43,7 +87,7 @@ UID[i]=monModuleRFID.serNum[i];
                  lcd.print("Leo                  ");
               lcd.setCursor(0,1);   
                  lcd.print("                     ");
-                 bilibip();
+                 here = bilibip(here);
              }
              else {
               lcd.setCursor(0,0);   
@@ -55,6 +99,7 @@ UID[i]=monModuleRFID.serNum[i];
                     lcd.print(UID[i],DEC);
                     lcd.print(".");
                 }
+                badcard();
           
             }       
           }   
@@ -64,10 +109,22 @@ UID[i]=monModuleRFID.serNum[i];
 }
 
 
-void bilibip()
+int bilibip(int here)
 {
-  tone(8, N1, 300);
-  tone(8, N2, 300);
-  tone(8, N3, 300);
+  if (here > melody_len-6) here = 0;
+    for (int thisNote = here; thisNote < here+6; thisNote++) {
+    int noteDuration = 1000 / noteDurations[thisNote];
+    tone(8, melody[thisNote], noteDuration);
+    int pauseBetweenNotes = noteDuration * 1.45;
+    delay(pauseBetweenNotes);
+    noTone(8);
+  }
+  return here+6;
   
+}
+
+
+void badcard()
+{
+  tone(8, N_F3, 500);
 }
