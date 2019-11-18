b92b9a800ce922b432308c50a1d977bde1167813
Lightman <leoguichar@gmail.com>
2019-11-18 10:32:29 +0100

    début de keypad

---
+++ projet technologique/projet/clavier/test1.0/test1.0.ino
@@ -0,0 +1,166 @@
+//YWROBOT
+//Compatible with the Arduino IDE 1.0
+//Library version:1.1
+#include <Wire.h> 
+#include <LiquidCrystal_I2C.h>
+#include <SPI.h>
+#include <RFID.h>
+#include <Keypad.h> // inclusion de la librairie pour clavier matriciel
+
+#define N_F3  175
+#define N_FS3 185
+#define N_G3  196
+#define N_GS3 208
+#define N_A3  220
+#define N_AS3 233
+#define N_B3  247
+#define N_C4  262
+#define N_CS4 277
+#define N_D4  294
+#define N_DS4 311
+#define N_E4  330
+#define N_F4  349
+#define N_FS4 370
+#define N_G4  392
+#define N_GS4 415
+#define N_A4  440
+#define N_AS4 466
+#define N_B4  494
+#define N_C5  523
+#define N_CS5 554
+#define N_D5  587
+#define N_DS5 622
+#define N_E5  659
+#define N_F5  698
+#define N_FS5 740
+#define N_G5  784
+#define N_GS5 831
+#define N_A5  880
+#define N_AS5 932
+#define N_B5  988
+#define N_C6  1047
+#define N_CS6 1109
+
+RFID monModuleRFID(10,9);
+LiquidCrystal_I2C lcd(0x27,20,4);  // set the LCD address to 0x27 for a 16 chars and 2 line display
+
+int UID[5];
+
+// notes in the melody:
+int melody[] = {
+  N_AS4, 0, 0, N_AS4, N_AS4, N_AS4, N_AS4, N_AS4, 0, N_GS4, N_AS4, 0, 0, N_AS4, N_AS4, N_AS4, N_AS4, N_AS4, 0, N_GS4, N_AS4, 0, 0, N_AS4, N_AS4, N_AS4, N_AS4, N_AS4, N_F3, N_F3, N_F3, N_F3, N_F3, N_F3, N_F3, N_F3, N_F3, N_F3, N_AS4, N_F3, N_F3, 0, N_AS4, N_AS4, N_C5, N_D5, N_DS5, N_F5, 0, N_F5, N_F5, N_FS5, N_GS5, N_AS5, 0, N_AS5, N_AS5, N_AS5, N_GS5, N_FS5, N_GS5, 0, N_FS5, N_F5, N_F5, N_DS5, N_DS5, N_F5, N_FS5, N_F5, N_DS5, N_CS5, N_CS5, N_DS5, N_F5, N_DS5, N_CS5, N_C5, N_C5, N_D5, N_E5, N_G5, N_F5, N_F3, N_F3, N_F3, N_F3, N_F3, N_F3, N_F3, N_F3, N_F3, N_F3, N_AS4, N_F3, N_F3, 0, N_AS4, N_AS4, N_C5, N_D5, N_DS5, N_F5, 0, N_F5, N_F5, N_FS5, N_GS5, N_AS5, 0, N_CS6, N_C6, N_A5, 0, N_F5, N_FS5, 0, N_AS5, N_A5, N_F5, 0, N_F5, N_FS5, 0, N_AS5, N_A5, N_F5, 0, N_D5, N_DS5, 0, N_FS5, N_F5, N_CS5, 0, N_AS4, N_C5, N_C5, N_D5, N_E5, 0, N_G5, N_F5, N_F3, N_F3, N_F3, N_F3, N_F3, N_F3, N_F3, N_F3, N_F3, N_F3, N_AS4, N_F3, N_F3, 0, N_AS4, N_AS4, N_C5, N_D5, N_DS5, N_F5, 0, N_F5, N_F5, N_FS5, N_GS5, N_AS5, 0, N_AS5, N_AS5, N_AS5, N_GS5, N_FS5, N_GS5, 0, N_FS5, N_F5, N_F5, N_DS5, N_DS5, N_F5, N_FS5, N_F5, N_DS5, N_CS5, N_CS5, N_DS5, N_F5, N_DS5, N_CS5, N_C5, N_C5, N_D5, N_E5, N_G5, N_F5, N_F3, N_F3, N_F3, N_F3, N_F3, N_F3, N_F3, N_F3, N_F3, N_F3, N_AS4, N_F3, N_F3, 0, N_AS4, N_AS4, N_C5, N_D5, N_DS5, N_F5, 0, N_F5, N_F5, N_FS5, N_GS5, N_AS5, 0, N_CS6, N_C6, N_A5, 0, N_F5, N_FS5, 0, N_AS5, N_A5, N_F5, 0, N_F5, N_FS5, 0, N_AS5, N_A5, N_F5, 0, N_D5, N_DS5, 0, N_FS5, N_F5, N_CS5, 0, N_AS4, N_C5, N_C5, N_D5, N_E5, 0, N_G5, N_F5, N_F3, N_F3, N_F3, N_F3, N_F3, N_F3, N_F3, N_F3, N_F3, N_F3
+};
+
+// note durations: 4 = quarter note, 8 = eighth note, etc.:
+int noteDurations[] = {
+  2, 8, 8, 8, 8, 8, 8, 6, 16, 16, 4, 8, 8, 8, 8, 8, 8, 6, 16, 16, 4, 8, 8, 8, 8, 8, 8, 8, 16, 16, 8, 16, 16, 8, 16, 16, 8, 8, 4, 4, 6, 16, 16, 16, 16, 16, 16, 2, 8, 8, 8 ,8 ,8, 2, 8, 8, 8 ,8 ,8, 8, 6, 16, 16, 2, 4, 8, 16, 16, 2, 8, 8, 8, 16, 16, 2, 8, 8, 8, 16, 16, 2, 4, 8, 16, 16, 8, 16, 16, 8, 16, 16, 8, 8, 4, 4, 6, 16, 16, 16, 16, 16, 16, 2, 8, 8, 8 ,8 ,8, 2, 4, 4, 4, 4, 4, 4, 2, 4, 4, 4, 4, 4, 4, 2, 4, 4, 4, 4, 4, 4, 2, 4, 4, 4, 4, 4, 4, 8, 16, 16, 4, 4, 4, 8, 16, 16, 8, 16, 16, 8, 16, 16, 8, 8, 4, 4, 6, 16, 16, 16, 16, 16, 16, 2, 8, 8, 8 ,8 ,8, 2, 8, 8, 8 ,8 ,8, 8, 6, 16, 16, 2, 4, 8, 16, 16, 2, 8, 8, 8, 16, 16, 2, 8, 8, 8, 16, 16, 2, 4, 8, 16, 16, 8, 16, 16, 8, 16, 16, 8, 8, 4, 4, 6, 16, 16, 16, 16, 16, 16, 2, 8, 8, 8 ,8 ,8, 2, 4, 4, 4, 4, 4, 4, 2, 4, 4, 4, 4, 4, 4, 2, 4, 4, 4, 4, 4, 4, 2, 4, 4, 4, 4, 4, 4, 8, 16, 16, 4, 4, 4, 8, 16, 16, 8, 16, 16, 8, 16, 16, 8, 8  
+};
+
+int melody_len = sizeof(melody)/sizeof(melody[0]);
+int here = 0;
+
+//--- Constantes utilisées avec le clavier 4x4
+const byte LIGNES = 4; // 4 lignes
+const byte COLONNES = 4; //4 colonnes
+
+
+const int C4=2; //declaration constante de broche
+const int C3=3; //declaration constante de broche
+const int C2=4; //declaration constante de broche
+const int C1=5; //declaration constante de broche
+
+const int L4=16; //declaration constante de broche
+const int L3=17; //declaration constante de broche
+const int L2=18; //declaration constante de broche
+const int L1=19; //declaration constante de broche
+
+
+//--- Définition des touches
+char touches[LIGNES][COLONNES] = {
+  {'1','2','3','A'},
+  {'4','5','6','B'},
+  {'7','8','9','C'},
+  {'*','0','#','D'}
+};
+
+// tableaux de lignes et colonnes
+byte BrochesLignes[LIGNES] = {L1, L2, L3, L4}; //connexions utilisées pour les broches de lignes du clavier
+byte BrochesColonnes[COLONNES] = {C1, C2, C3, C4}; //connexions utilisées pour les broches de colonnes du clavier
+
+char touche; // variable de stockage valeur touche appuyée
+
+// création d'un objet keypad = initialisation clavier
+Keypad clavier = Keypad( makeKeymap(touches), BrochesLignes, BrochesColonnes, LIGNES, COLONNES );
+// les broches de lignes sont automatiquement configurées en ENTREE avec pullup interne activé
+// les broches de colonnes sont automatiquement configurées en SORTIE
+
+void setup()
+{
+  lcd.init();                      // initialize the lcd 
+  // Print a message to the LCD.
+  lcd.setCursor(0,0);
+  lcd.print("Hello, world!");
+
+  
+    SPI.begin();
+  monModuleRFID.init();  
+
+}
+
+
+void loop()
+{
+             
+      if (monModuleRFID.isCard()) {  
+          if (monModuleRFID.readCardSerial()) {     
+                            for(int i=0;i<=4;i++)
+                {
+UID[i]=monModuleRFID.serNum[i];
+                }
+            
+             if (UID[0] == 13){
+              lcd.setCursor(0,0);   
+                 lcd.print("Leo                  ");
+              lcd.setCursor(0,1);   
+                 lcd.print("                     ");
+                 here = bilibip(here);
+             }
+             else {
+              lcd.setCursor(0,0);   
+              lcd.print("carte non reconnue");
+                  lcd.setCursor(0,1);   
+                for(int i=0;i<=4;i++)
+                {
+                    
+                    lcd.print(UID[i],DEC);
+                    lcd.print(".");
+                }
+                badcard();
+          
+            }       
+          }   
+          monModuleRFID.halt();
+    }
+    delay(1);    
+}
+
+
+int bilibip(int here)
+{
+  if (here > melody_len-6) here = 0;
+    for (int thisNote = here; thisNote < here+6; thisNote++) {
+    int noteDuration = 1000 / noteDurations[thisNote];
+    tone(8, melody[thisNote], noteDuration);
+    int pauseBetweenNotes = noteDuration * 1.45;
+    delay(pauseBetweenNotes);
+    noTone(8);
+  }
+  return here+6;
+  
+}
+
+
+void badcard()
+{
+  tone(8, N_F3, 500);
+}
---
+++ projet technologique/projet/clavier/test3.0/test3.0.ino
@@ -0,0 +1,154 @@
+//YWROBOT
+//Compatible with the Arduino IDE 1.0
+//Library version:1.1
+#include <Wire.h> 
+#include <LiquidCrystal_I2C.h>
+#include <SPI.h>
+#include <RFID.h>
+#include <Keypad.h>
+
+
+#define N_F3  175
+#define N_FS3 185
+#define N_G3  196
+#define N_GS3 208
+#define N_A3  220
+#define N_AS3 233
+#define N_B3  247
+#define N_C4  262
+#define N_CS4 277
+#define N_D4  294
+#define N_DS4 311
+#define N_E4  330
+#define N_F4  349
+#define N_FS4 370
+#define N_G4  392
+#define N_GS4 415
+#define N_A4  440
+#define N_AS4 466
+#define N_B4  494
+#define N_C5  523
+#define N_CS5 554
+#define N_D5  587
+#define N_DS5 622
+#define N_E5  659
+#define N_F5  698
+#define N_FS5 740
+#define N_G5  784
+#define N_GS5 831
+#define N_A5  880
+#define N_AS5 932
+#define N_B5  988
+#define N_C6  1047
+#define N_CS6 1109
+
+int UID[5];
+
+// notes in the melody:
+int melody[] = {
+  N_AS4, 0, 0, N_AS4, N_AS4, N_AS4, N_AS4, N_AS4, 0, N_GS4, N_AS4, 0, 0, N_AS4, N_AS4, N_AS4, N_AS4, N_AS4, 0, N_GS4, N_AS4, 0, 0, N_AS4, N_AS4, N_AS4, N_AS4, N_AS4, N_F3, N_F3, N_F3, N_F3, N_F3, N_F3, N_F3, N_F3, N_F3, N_F3, N_AS4, N_F3, N_F3, 0, N_AS4, N_AS4, N_C5, N_D5, N_DS5, N_F5, 0, N_F5, N_F5, N_FS5, N_GS5, N_AS5, 0, N_AS5, N_AS5, N_AS5, N_GS5, N_FS5, N_GS5, 0, N_FS5, N_F5, N_F5, N_DS5, N_DS5, N_F5, N_FS5, N_F5, N_DS5, N_CS5, N_CS5, N_DS5, N_F5, N_DS5, N_CS5, N_C5, N_C5, N_D5, N_E5, N_G5, N_F5, N_F3, N_F3, N_F3, N_F3, N_F3, N_F3, N_F3, N_F3, N_F3, N_F3, N_AS4, N_F3, N_F3, 0, N_AS4, N_AS4, N_C5, N_D5, N_DS5, N_F5, 0, N_F5, N_F5, N_FS5, N_GS5, N_AS5, 0, N_CS6, N_C6, N_A5, 0, N_F5, N_FS5, 0, N_AS5, N_A5, N_F5, 0, N_F5, N_FS5, 0, N_AS5, N_A5, N_F5, 0, N_D5, N_DS5, 0, N_FS5, N_F5, N_CS5, 0, N_AS4, N_C5, N_C5, N_D5, N_E5, 0, N_G5, N_F5, N_F3, N_F3, N_F3, N_F3, N_F3, N_F3, N_F3, N_F3, N_F3, N_F3, N_AS4, N_F3, N_F3, 0, N_AS4, N_AS4, N_C5, N_D5, N_DS5, N_F5, 0, N_F5, N_F5, N_FS5, N_GS5, N_AS5, 0, N_AS5, N_AS5, N_AS5, N_GS5, N_FS5, N_GS5, 0, N_FS5, N_F5, N_F5, N_DS5, N_DS5, N_F5, N_FS5, N_F5, N_DS5, N_CS5, N_CS5, N_DS5, N_F5, N_DS5, N_CS5, N_C5, N_C5, N_D5, N_E5, N_G5, N_F5, N_F3, N_F3, N_F3, N_F3, N_F3, N_F3, N_F3, N_F3, N_F3, N_F3, N_AS4, N_F3, N_F3, 0, N_AS4, N_AS4, N_C5, N_D5, N_DS5, N_F5, 0, N_F5, N_F5, N_FS5, N_GS5, N_AS5, 0, N_CS6, N_C6, N_A5, 0, N_F5, N_FS5, 0, N_AS5, N_A5, N_F5, 0, N_F5, N_FS5, 0, N_AS5, N_A5, N_F5, 0, N_D5, N_DS5, 0, N_FS5, N_F5, N_CS5, 0, N_AS4, N_C5, N_C5, N_D5, N_E5, 0, N_G5, N_F5, N_F3, N_F3, N_F3, N_F3, N_F3, N_F3, N_F3, N_F3, N_F3, N_F3
+};
+
+// note durations: 4 = quarter note, 8 = eighth note, etc.:
+int noteDurations[] = {
+  2, 8, 8, 8, 8, 8, 8, 6, 16, 16, 4, 8, 8, 8, 8, 8, 8, 6, 16, 16, 4, 8, 8, 8, 8, 8, 8, 8, 16, 16, 8, 16, 16, 8, 16, 16, 8, 8, 4, 4, 6, 16, 16, 16, 16, 16, 16, 2, 8, 8, 8 ,8 ,8, 2, 8, 8, 8 ,8 ,8, 8, 6, 16, 16, 2, 4, 8, 16, 16, 2, 8, 8, 8, 16, 16, 2, 8, 8, 8, 16, 16, 2, 4, 8, 16, 16, 8, 16, 16, 8, 16, 16, 8, 8, 4, 4, 6, 16, 16, 16, 16, 16, 16, 2, 8, 8, 8 ,8 ,8, 2, 4, 4, 4, 4, 4, 4, 2, 4, 4, 4, 4, 4, 4, 2, 4, 4, 4, 4, 4, 4, 2, 4, 4, 4, 4, 4, 4, 8, 16, 16, 4, 4, 4, 8, 16, 16, 8, 16, 16, 8, 16, 16, 8, 8, 4, 4, 6, 16, 16, 16, 16, 16, 16, 2, 8, 8, 8 ,8 ,8, 2, 8, 8, 8 ,8 ,8, 8, 6, 16, 16, 2, 4, 8, 16, 16, 2, 8, 8, 8, 16, 16, 2, 8, 8, 8, 16, 16, 2, 4, 8, 16, 16, 8, 16, 16, 8, 16, 16, 8, 8, 4, 4, 6, 16, 16, 16, 16, 16, 16, 2, 8, 8, 8 ,8 ,8, 2, 4, 4, 4, 4, 4, 4, 2, 4, 4, 4, 4, 4, 4, 2, 4, 4, 4, 4, 4, 4, 2, 4, 4, 4, 4, 4, 4, 8, 16, 16, 4, 4, 4, 8, 16, 16, 8, 16, 16, 8, 16, 16, 8, 8  
+};
+
+int melody_len = sizeof(melody)/sizeof(melody[0]);
+int here = 0;
+
+const byte ROWS = 4; //four rows
+const byte COLS = 4; //four columns
+//define the cymbols on the buttons of the keypads
+char hexaKeys[ROWS][COLS] = {
+  {'1','2','3','A'},
+  {'4','5','6','B'},
+  {'7','8','9','C'},
+  {'*','0','#','D'}
+};
+byte rowPins[ROWS] = {2,3 ,4 ,5 }; //connect to the row pinouts of the keypad
+byte colPins[COLS] = {A0, A1, A2,A3}; //connect to the column pinouts of the keypad
+
+RFID monModuleRFID(10,9);
+LiquidCrystal_I2C lcd(0x27,20,4);  // set the LCD address to 0x27 for a 16 chars and 2 line display
+Keypad customKeypad = Keypad( makeKeymap(hexaKeys), rowPins, colPins, ROWS, COLS); 
+
+
+void setup()
+{
+  lcd.init();                      // initialize the lcd 
+  // Print a message to the LCD.
+  lcd.setCursor(0,0);
+  lcd.print("Hello, world!");
+
+  
+  Serial.begin(9600);
+
+  
+    SPI.begin();
+  monModuleRFID.init();  
+
+}
+
+
+void loop()
+{
+    char customKey = customKeypad.getKey();
+  
+  if (customKey){
+    Serial.println(customKey);
+  }
+             
+      if (monModuleRFID.isCard()) {  
+          if (monModuleRFID.readCardSerial()) {     
+                            for(int i=0;i<=4;i++)
+                {
+UID[i]=monModuleRFID.serNum[i];
+                }
+            
+             if (UID[0] == 13){
+              lcd.setCursor(0,0);   
+                 lcd.print("Leo                  ");
+              lcd.setCursor(0,1);   
+                 lcd.print("                     ");
+                 here = bilibip(here);
+             }
+             else {
+              lcd.setCursor(0,0);   
+              lcd.print("carte non reconnue");
+                  lcd.setCursor(0,1);   
+                for(int i=0;i<=4;i++)
+                {
+                    
+                    lcd.print(UID[i],DEC);
+                    lcd.print(".");
+                }
+                badcard();
+          
+            }       
+          }   
+          monModuleRFID.halt();
+    }
+    delay(1);    
+}
+
+
+int bilibip(int here)
+{
+  if (here > melody_len-6) here = 0;
+    for (int thisNote = here; thisNote < here+6; thisNote++) {
+    int noteDuration = 1000 / noteDurations[thisNote];
+    tone(8, melody[thisNote], noteDuration);
+    int pauseBetweenNotes = noteDuration * 1.45;
+    delay(pauseBetweenNotes);
+    noTone(8);
+  }
+  return here+6;
+  
+}
+
+
+void badcard()
+{
+  tone(8, N_F3, 500);
+}
