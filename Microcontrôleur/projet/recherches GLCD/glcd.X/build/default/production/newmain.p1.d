7743af49e8316c67a8a39cb808bb78be6c015116
Lightman <leoguichar@gmail.com>
2019-11-22 09:31:23 +0100

    librairies et code prof

---
+++ Microcontrôleur/projet/recherches GLCD/glcd.X/build/default/production/newmain.p1.d
@@ -0,0 +1,6 @@
+ build/default/production/newmain.d  \
+ build/default/production/newmain.p1:  \
+ newmain.c  \
+init.h  \
+glcd.h  \
+main.h 
---
+++ Microcontrôleur/projet/recherches GLCD/testCLCD.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=testCLCD.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/testCLCD.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=testclcd.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/testclcd.x.tar
