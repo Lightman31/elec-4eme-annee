c9940f3a3e03c89acb0938a726699b2e9233143c
Lightman <leoguichar@gmail.com>
2019-11-29 05:48:33 +0100

    Ca maaaaaaaarche enfin !

---
+++ Microcontrôleur/projet/GlcdOKDébutTetris/Faut_que_ca_marche.X/newmain1.c
@@ -0,0 +1,68 @@
+/* 
+ * File:   newmain.c
+ * Author: Adrian Chazottes
+ *
+ * Created on 18 novembre 2016, 10:00
+ */
+
+#include "main.h"
+
+#pragma config FOSC = HS 		//oscillator HS
+#pragma config PWRT = OFF
+#pragma config BOR = OFF
+#pragma config WDT = OFF 		//Disable watchDog
+#pragma config LVP = OFF 		//Disable low voltage programmig
+#pragma config DEBUG = ON		//Debug ON
+
+
+void main (void)
+{
+    // call the function that will init the PIC
+    initMyPIC18F();
+    glcd_Init(GLCD_ON);
+    //glcd_PlotPixel( 2, 2, 0xFF);
+    
+    int i = 0;
+    unsigned char ecran[10][22] = {0};
+    
+    ecran[0][21] = 1;
+    ecran[9][21] = 1;
+    ecran[1][21] = 1;
+    ecran[0][0] = 1;
+    
+    
+    
+    ecran[5][10] = 1;
+    ecran[5][11] = 1;
+    ecran[5][12] = 1;
+    ecran[6][12] = 1;
+    
+    // Swith the screen ON
+
+    // Set the cursor and write a string
+    glcd_SetCursor(1, 0);
+
+//    glcd_PlotPixel(10,0,1);
+//    glcd_PlotPixel(40,0,1);
+//    glcd_PlotPixel(10,60,1);
+//    glcd_PlotPixel(40,60,1);
+    glcd_Rect(1,21,90,62,1);
+    glcd_print_tab(ecran);
+    
+    while(1)
+    {
+        ecran[i][i] = 1;
+        
+    glcd_print_tab(ecran);
+            
+        ecran[i][i] = 0;
+        i++;
+        if (i == 10) i = 0;
+    }
+
+}// end of the main function
+
+// make sure to have an empty LAST line in any *.c file (just hit an Enter)!
+
+
+
---
+++ Microcontrôleur/projet/recherches GLCD/Faut_que_ca_marche.X/build/default/production/newmain1.p1.d
@@ -0,0 +1,5 @@
+build/default/production/newmain1.p1:  \
+newmain1.c  \
+main.h  \
+glcd.h  \
+init.h 
