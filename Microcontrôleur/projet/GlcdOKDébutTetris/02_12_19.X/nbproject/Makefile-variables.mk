79455cc9b131f8b58232e1b7a168f09fda2a824d
Lightman <leoguichar@gmail.com>
2019-12-02 14:30:59 +0100

    V2

---
+++ Microcontrôleur/projet/GlcdOKDébutTetris/02_12_19.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=02_12_19.X.hex
+CND_ARTIFACT_PATH_default=dist/default/production/02_12_19.X.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=021219.X.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/021219.X.tar
---
+++ Microcontrôleur/projet/GlcdOKDébutTetris/02_12_19.X/newmain1.c
@@ -0,0 +1,91 @@
+/* 
+ * File:   newmain.c
+ * Author: Adrian Chazottes
+ *
+ * Created on 18 novembre 2016, 10:00
+ */
+
+#include "main.h"
+
+#pragma config FOSC = HS 		//oscillator HS
+#pragma config PWRT = OFF
+#pragma config BOR = OFF
+#pragma config WDT = OFF 		//Disable watchDog
+#pragma config LVP = OFF 		//Disable low voltage programmig
+#pragma config DEBUG = ON		//Debug ON
+
+
+void main (void)
+{
+    // call the function that will init the PIC
+    initMyPIC18F();
+    glcd_Init(GLCD_ON);
+    //glcd_PlotPixel( 2, 2, 0xFF);
+    
+    int i = 0;
+                
+    int x,y;
+    unsigned char ecran[10][22] = {0};
+    unsigned char forme[4][2] = {0};
+    unsigned char previous_forme[4][2] = {0};
+    
+    ecran[0][21] = 1;
+    ecran[9][21] = 1;
+    ecran[1][21] = 1;
+    ecran[0][0] = 1;
+    
+    forme[0][0] = 5;
+    forme[1][0] = 6;
+    forme[2][0] = 7;
+    forme[3][0] = 7;
+    forme[0][1] = 5;
+    forme[1][1] = 5;
+    forme[2][1] = 5;
+    forme[3][1] = 6;
+    
+    
+    
+    ecran[5][10] = 1;
+    ecran[5][11] = 1;
+    ecran[5][12] = 1;
+    ecran[6][12] = 1;
+    
+    // Swith the screen ON
+
+    // Set the cursor and write a string
+    glcd_SetCursor(1, 0);
+
+//    glcd_PlotPixel(10,0,1);
+//    glcd_PlotPixel(40,0,1);
+//    glcd_PlotPixel(10,60,1);
+//    glcd_PlotPixel(40,60,1);
+    glcd_Rect(1,21,90,62,1);
+    glcd_print_tab(ecran);
+    
+    while(1)
+    {
+        glcd_draw_shape(previous_forme,0);
+            i++;
+        glcd_draw_shape(forme,1);
+              
+  
+
+
+
+         for(y=0;y<=3;y++)
+           {
+               previous_forme[y][0] =  forme[y][x];
+           }
+      
+        
+        
+        if (i == 18) i = 0;
+        __delay_us(1000000);
+    }
+
+}// end of the main function
+
+// make sure to have an empty LAST line in any *.c file (just hit an Enter)!
+
+
+
