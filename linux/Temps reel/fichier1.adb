c179de0a93fdda0af734d271317d17d98001aece
Lightman <leoguich@yahoo.fr>
2020-03-25 15:29:43 +0100

    Code compilé

--- linux/Temps reel/fichier1.adb
+++ linux/Temps reel/fichier1.adb
@@ -6,18 +6,18 @@ procedure Main is
         begin
             loop
                 Put_line("A");
-                delay 1.0;
+                --delay 1.0;
                 Put_line("B");
-                delay 1.0;
+                --delay 1.0;
             end loop;
     end displayLetter1;
     task body displayLetter2 is
         begin
             loop
                 Put_line("C");
-                delay 1.0;
+                --delay 1.0;
                 Put_line("D");
-                delay 1.0 ;
+                --delay 1.0 ;
             end loop;
     end displayLetter2;
 begin
