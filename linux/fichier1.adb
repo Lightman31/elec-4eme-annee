c58d9d26d2cd04f8aaca1999f860135c9ade9bc3
Adrien Buot <adrien.buot@edu.ece.fr>
2020-03-25 15:07:24 +0100

    Premier fichier adb pour tp 1

---
+++ linux/fichier1.adb
@@ -0,0 +1,25 @@
+with Text_IO; use Text_IO;
+procedure Main is
+    task displayLetter1;
+    task displayLetter2;
+    task body displayLetter1 is
+        begin
+            loop
+                Put_line("A");
+                delay(1.0);
+                Put_line("B");
+                delay(1.0);
+            end loop;
+    end displayLetter1;
+    task body displayLetter2 is
+        begin
+            loop
+                Put_line("C");
+                delay(1.0);
+                Put_line("D");
+                delay(1.0);
+            end loop;
+    end displayLetter2;
+begin
+    null;
+end Main;
